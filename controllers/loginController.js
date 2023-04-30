62c890fca9ea80faac9c82998a5a7442697136e4
Kavya HT <kavyaht39@gmail.com>
2023-04-30 21:34:36 +0530

    completed designing the frontend and backend of login page

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -11,9 +11,9 @@ exports.postLogin=async function(req,res,next){
   let responseEmail=await users.findOne({where:{email:req.body.email}});
   let responsePassword=await users.findOne({where:{password:password}})
   if(responseEmail === null && responsePassword === null){
-    res.json("The user is not found");
+   res.json('The user does not exist')
   }else if(responseEmail === null){
-    res.json('The user does not exist')
+    res.json('The email does not exist')
   }else if(responsePassword ===null){
    res.json('The pasword does not exist');
   }
