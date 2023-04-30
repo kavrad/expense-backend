0646427ddd694ad997b766f78e5e74f010ae9f9e
Kavya HT <kavyaht39@gmail.com>
2023-04-30 18:29:28 +0530

    Login frontend completed successfully

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express=require('express');
 const path=require('path');
 const bodyParser=require('body-parser');
+const sequelize=require('./utils/database');
 const port=800;
 const server=express();
 
-server.use(bodyParser.urlencoded({extended:false}));
+server.use(bodyParser.json());
 
 server.use(express.static(path.join(__dirname,'public')));
 
@@ -14,13 +15,20 @@ server.get('/',function(req,res,next){
 
 server.post('/add-user',require('./controllers/signUpController').signUp);
 
-server.listen(port,function(err){
-    try{
-        if(err){
-            throw err;
+server.get('/login',require('./controllers/loginController').login);
+
+server.post('/user/login',require('./controllers/loginController').postLogin);
+
+sequelize.sync().then((result)=>{
+    console.log(result);
+    server.listen(port,function(err){
+        try{
+            if(err){
+                throw err;
+            }
+            console.log(`Server is running on port ${port}`);
+        }catch(err){
+            console.log(err);
         }
-        console.log(`Server is running on port ${port}`);
-    }catch(err){
-        console.log(err);
-    }
-})
+    })
+}).catch(err => console.log(err));
--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,5 +1,23 @@
 const path=require('path');
+const users=require('../models/users');
+
 
 exports.login=function(req,res,next){
     res.sendFile(path.join(__dirname,'..','views','login.html'))
+}
+exports.postLogin=async function(req,res,next){
+    const email=req.body.email;
+    const password=req.body.password;
+  let responseEmail=await users.findOne({where:{email:req.body.email}});
+  let responsePassword=await users.findOne({where:{password:password}})
+  if(responseEmail === null && responsePassword === null){
+    res.json("The user is not found");
+  }else if(responseEmail === null){
+    res.json('The user does not exist')
+  }else if(responsePassword ===null){
+   res.json('The pasword does not exist');
+  }
+  else{
+    res.json(responseEmail);
+  }
 }
