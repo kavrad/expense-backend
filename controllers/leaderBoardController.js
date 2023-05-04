5c187971a03764575518df6065ceb3017c084600
Kavya HT <kavyaht39@gmail.com>
2023-05-04 19:02:32 +0530

    OPtimised the leaderboard backend

--- controllers/leaderBoardController.js
+++ controllers/leaderBoardController.js
@@ -1,27 +1,22 @@
 const users=require('../models/users');
 const expenses=require('../models/expense');
+const sequelize = require('../utils/database');
+
 exports.getLeaderBoard=async function(req,res,next){
     try{
-      const Users=await users.findAll();
-      const Expenses=await expenses.findAll();
-      console.log(Expenses)
-      const groupExpenses={};
-      Expenses.forEach(function(expense){
-       if(groupExpenses[expense.userId]){
-        groupExpenses[expense.userId]=groupExpenses[expense.userId]+expense.expenseAmount;
-       }else{
-        groupExpenses[expense.userId]=expense.expenseAmount;
-       }
-      })
-      console.log(groupExpenses);
-      const groupUsers=[];
-      Users.forEach(function(user){
-       groupUsers.push({name:user.name,amount:groupExpenses[user.id] || 0})
-      })
-    let newUserleaderBoard=groupUsers.sort(function(a,b){
-       return b.amount-a.amount;
-      })
-      return res.status(200).json(newUserleaderBoard);
+      const Users=await users.findAll({
+        attributes:['id','name',[sequelize.fn('sum',sequelize.col(`expenses.expenseAmount`)),'amount']],
+        include:[
+        {
+            model:expenses,
+            attributes:[]
+        }
+        ],
+        group:['users.id'],
+        order:[[sequelize.col('amount'),"DESC"]]
+    });
+    
+      return res.status(200).json(Users);
     }catch(err){
         console.log(err);
         res.status(500).json({message:err,status:"failed"})
