52284a5868c46113fc10e542e3419b595d8b8a52
Kavya HT <kavyaht39@gmail.com>
2023-05-05 17:13:56 +0530

    completed with transactions to add and delete expense

--- controllers/addExpensesController.js
+++ controllers/addExpensesController.js
@@ -1,28 +1,34 @@
 const path=require('path');
 const expenses=require('../models/expense');
 const users=require('../models/users');
+const sequelize=require('../utils/database')
+
 exports.addExpense=function(req,res,next){
 res.sendFile(path.join(__dirname,'..','views','addExpenses.html'));
 }
 
 exports.postAddExpense=async function(req,res,next){
-   console.log(req.body);
-       expenses.create({
+   const t=await sequelize.transaction();
+   try{
+     console.log(req.body);
+       let expenses=await expenses.create({
          expenseAmount:req.body.expenseAmount,
          desc:req.body.desc,
          category:req.body.category,
          userId:req.user.id,
          
-      }).then(()=>{
+      },{transaction:t})
          const totalExpense=Number(req.user.totalExpenses)+Number(req.body.expenseAmount);
          console.log(totalExpense);
-         users.update({totalExpenses:totalExpense},{where:{id:req.user.id}}).then(()=>{
-            res.redirect('/show-expense');
-         }).catch((err)=>{
-            return res.status(500).json({message:err,status:"failed"})
-         })
-        
-      }).catch(err => console.log(err))
+         await users.update({totalExpenses:totalExpense},{where:{id:req.user.id}},{transaction:t})
+         await t.commit();
+         res.redirect('/show-expense');
+         
+      }catch(err){
+      t.rollback();
+      return res.status(500).json({message:err,status:"failed"})
+   }
+      
    }
 
 exports.showExpense=function(req,res,next){
--- controllers/deleteExpenseController.js
+++ controllers/deleteExpenseController.js
@@ -1,10 +1,25 @@
 const expenses=require('../models/expense');
-exports.deleteExpense=function(req,res,next){
+const users=require('../models/users');
+const sequelize=require('../utils/database');
+
+exports.deleteExpense= async function(req,res,next){
+  const t=await sequelize.transaction();
     const id=req.params.id;
+   const user= await users.findOne({where:{id:req.user.id}});
     expenses.findOne({where:{Id:id,userId:req.user.id}}).then((expense)=>{
-      expense.destroy().then(()=>{
+      const amount=Number(expense.expenseAmount);
+      const expenses=Number(user.totalExpenses);
+      const totalExpenses=expense>amount?expenses-amount:amount-expenses;
+      expense.destroy({transaction:t}).then(async ()=>{
+        await users.update({totalExpenses:totalExpenses},{where:{id:id}},{transaction:t})
+        await t.commit();
         res.json({expense:expense,message:'Are you sure you want to delete it??'});
-      }).catch(err => console.log(err));
+      }).catch(err => {
+        t.rollback();
+        console.log(err)
+
+      }
+      );
     })
 }
 
