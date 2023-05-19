a427ece23d7c3edea5b00a0032472adc8fb261c3
kavrad <105067853+kavrad@users.noreply.github.com>
2023-05-19 06:37:58 +0530

    Add files via upload

---
+++ utils/database.js
@@ -0,0 +1,10 @@
+const {Sequelize}=require('sequelize');
+
+require('dotenv').config();
+
+console.log(process.env);
+const sequelize=new Sequelize(process.env.SQL_DATABASE,process.env.SQL_USER,process.env.SQL_PASSWORD,{
+    dialect:'mysql',
+    host:process.env.SQL_HOST,
+});
+module.exports=sequelize;
